c4fc3e3f3c0bcbd08f77b962fc5ffd259d99bb0b
LalaValencia <lalavalencia777@gmail.com>
2025-01-25 00:16:11 -0500

    MenuInicio y puntaje
    Creacion escena basica del menu de inicio, y creacion del score

---
+++ GGJBilio/Assets/Scripts/Score.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public static Score Instance; // Referencia estática al ScoreManager
+    public TextMeshProUGUI scoreText; // El texto en el Canvas donde se mostrará el puntaje
+
+    private int currentScore = 0; // Puntaje actual
+
+    // Aseguramos que solo haya una instancia del ScoreManager
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject); // Si ya existe una instancia, destruimos la nueva
+        }
+    }
+
+    // Método para agregar puntaje
+    public void AddScore(int scoreToAdd)
+    {
+        currentScore += scoreToAdd;
+        UpdateScoreUI();
+    }
+
+    // Actualizamos el texto en el Canvas con el puntaje actual
+    private void UpdateScoreUI()
+    {
+        scoreText.text = "Score: " + currentScore.ToString();
+    }
+}
