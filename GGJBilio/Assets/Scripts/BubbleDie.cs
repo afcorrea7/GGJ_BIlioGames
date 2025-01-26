c2d42831457235e756e6ac8ced5211cb56b84a51
LalaValencia <lalavalencia777@gmail.com>
2025-01-25 23:29:11 -0500

    Crisis
    se anadieronn los sprites a los prefabs, se anadio y programo la explosion de la burbuja, se andieron los elementos graficos del menu

--- GGJBilio/Assets/Scripts/BubbleDie.cs
+++ GGJBilio/Assets/Scripts/BubbleDie.cs
@@ -7,14 +7,32 @@ public class BubbleDie : MonoBehaviour
 {
     public delegate void OnPlayerLose(); // Define el evento de perder
     public static event OnPlayerLose PlayerLost;
+
+    private Animator animator;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponentInParent<Rigidbody2D>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Verifica si el objeto colisionado tiene la etiqueta "Obstacle"
         if (collision.gameObject.CompareTag("Obstacle"))
         {
-            // Dispara el evento de perder
-            PlayerLost?.Invoke();
-            Debug.Log("El jugador ha perdido.");
+           rb.velocity = Vector2.zero;
+
+            if (animator != null)
+            {
+                animator.SetTrigger("Death");
+            }
+            StartCoroutine(DelayedInvoke());
         }
+    }
+    private IEnumerator DelayedInvoke()
+    {
+        yield return new WaitForSeconds(1f);
+        PlayerLost?.Invoke();
     }
 }
